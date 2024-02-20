3e844e1204d54c2c641e5e5b37bd22850d6f2b1e
dialop <dianalop1998@gmail.com>
2024-02-19 22:50:05 -0500

    Add build scripts in html file

---
+++ portfolio/frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Typography } from '@mui/material';
+
+function Home() {
+  return (
+    <div>
+      <Typography variant="h1">Welcome to My App</Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
