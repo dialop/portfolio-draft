47af33f88e5f56dd4d0ec69712ecb52b9f518b0b
dialop <dianalop1998@gmail.com>
2024-02-20 21:15:28 -0500

    Add homepage component

--- portfolio/frontend/src/components/Home.jsx
+++ portfolio/frontend/src/components/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { Button, Typography } from '@mui/material';
 
 function Home() {
+  const handleClick = () => {
+    alert('Button clicked!');
+    // You can add any other functionality you want here
+  };
+
   return (
     <div>
+      {/* Use Typography component directly inside the JSX */}
       <Typography variant="h1">Welcome to My App</Typography>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={handleClick}>
         Click Me
       </Button>
     </div>
